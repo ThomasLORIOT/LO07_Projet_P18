af25ffaba8517ab7150942e48cdf256748da03ee
Thomas LORIOT <thomas.loriot@utt.fr>
2018-06-06 09:55:02 +0200

    add objet utilisateur

--- Objets/Utilisateur.php
+++ Objets/Utilisateur.php
@@ -12,8 +12,43 @@
  * @author Thomas
  */
 class Utilisateur {
+
     private $nom;
     private $email;
     private $MDP;
-    
+
+    function getNom() {
+        return $this->nom;
+    }
+
+    function getEmail() {
+        return $this->email;
+    }
+
+    function getMDP() {
+        return $this->MDP;
+    }
+
+    function setNom($nom) {
+        $this->nom = $nom;
+    }
+
+    function setEmail($email) {
+        $this->email = $email;
+    }
+
+    function setMDP($MDP) {
+        $this->MDP = $MDP;
+    }
+
+    function __construct($nom,$email,$MDP){
+        $this->nom = $nom;
+        $this->email = $email;
+        $this->MDP = $MDP;
+    }
+
+    function set_password($value) {
+        $this->MDP = password_hash($value, PASSWORD_DEFAULT);
+    }
+
 }
