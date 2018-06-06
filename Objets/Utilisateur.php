9cd605c42ae754d2aa8b91a91173838ac6039ea0
Thomas LORIOT <thomas.loriot@utt.fr>
2018-06-06 09:31:35 +0200

    add object

---
+++ Objets/Utilisateur.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Utilisateur
+ *
+ * @author Thomas
+ */
+class Utilisateur {
+    private $nom;
+    private $email;
+    private $MDP;
+    
+}
