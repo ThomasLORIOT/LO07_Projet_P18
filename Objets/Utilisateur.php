41b05d6ed18037a00317bd1d9af5b08c62220b49
Thomas LORIOT <thomas.loriot@utt.fr>
2018-06-08 11:16:18 +0200

    update class utilisateur

--- Objets/Utilisateur.php
+++ Objets/Utilisateur.php
@@ -53,31 +53,62 @@ class Utilisateur {
         $this->MDP = password_hash($MDP, PASSWORD_DEFAULT);
     }
 
-    function __construct($nom, $email, $MDP) {
+    function __construct() {
+        $argv = func_get_args();
+        switch (func_num_args()) {
+            case 2:
+                self::__construct1($argv[0], $argv[1]);
+                break;
+            case 3:
+                self::__construct2($argv[0], $argv[1], $argv[2]);
+                break;
+        }
+    }
+
+    function __construct1($email, $MDP) {
+        $myDB = connectDB();
+        $result = $myDB->query("SELECT * FROM utilisateur WHERE Email = '$email'");
+        if ($result->num_rows == 0) {
+             echo "<script>console.log('connait pas ce mail');</script>";
+        }
+        $row = mysqli_fetch_row($result);
+        $hash = $row[2];
+
+        if (password_verify($MDP, $hash)) {
+            $this->idUtilisateur = $row[0];
+            $this->nom = $row[1];
+            $this->email = $row[3];
+            $this->MDP = $row[2];
+        } else {
+            echo "<script>console.log('mauvais mot de passe');</script>";
+        }
+    }
+
+    function __construct2($nom, $email, $MDP) {
         $this->nom = $nom;
         $this->email = $email;
         $this->setMDP($MDP);
     }
 
     function __tostring() {
-        return "Utilisateur($this->nom, $this->email, $this->MDP)<br>\n";
+        return "Utilisateur($this->idUtilisateur, $this->nom, $this->email, $this->MDP)<br>\n";
     }
-    
+
     //ajout d'un utilisateur dans la base
     function addDB() {
         //connexion à la base
-        $myDB= connectDB();
+        $myDB = connectDB();
         //est-ce que le mail existe dejà
-        $result=$myDB->query("SELECT * FROM utilisateur WHERE Email='$this->email'");
-        if($result->num_rows==0){ //si non 
-            $requete="INSERT INTO utilisateur(nom,email,MDP) VALUES ('$this->nom','$this->email','$this->MDP')";
+        $result = $myDB->query("SELECT * FROM utilisateur WHERE Email='$this->email'");
+        if ($result->num_rows == 0) { //si non 
+            $requete = "INSERT INTO utilisateur(nom,email,MDP) VALUES ('$this->nom','$this->email','$this->MDP')";
             //echo($requete);
-            if($myDB->query($requete)==TRUE){
-                echo("Insertion réussie<br>");
-            }else{
-                printf("Error: %s <br> " ,$myDB->error);
+            if ($myDB->query($requete) == TRUE) {
+                echo("Insertion réussie<br>"); //popup qui dira inscription reussi
+            } else {
+                printf("Error: %s <br> ", $myDB->error);
             }
-        }else{ // si oui 
+        } else { // si oui 
             echo("Ajout de l'utilisateur impossible : mail existant<br>");
         }
         mysqli_close($myDB);
@@ -89,22 +120,9 @@ class Utilisateur {
 
 }
 
-$test = new Utilisateur("test", "vlad@bg.fr", "lol");
-$test->addDB();
-
-$test1 = new Utilisateur("test", "rerfct@bg.fr", "lol");
-$test1->addDB();
-//echo($test->getIdUtilisateur());
-//echo($test);
-//  echo($test->getIdUtilisateur());
-//echo(debug($test->getIdUtilisateur()));
+$test = new Utilisateur("yolo", "yolo@rer.fr", "lsqsqdqs");
+echo($test);
+$test2 = new Utilisateur("vlad@bg.fr", "lol");
+echo($test2);
 
 
-//$requete="SELECT * FROM utilisateur WHERE Email='test@test.test'";
-//$result=$myDB->query($requete);
-//echo($result->num_rows);
-
-$myDB= connectDB();
-$requete="SELECT * FROM utilisateur";
-$result=$myDB->query($requete);
-echo("Nombre de ligne pour la requete : <br> $requete = : $result->num_rows");
