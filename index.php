cc18de896028bf8f500346dd44b0cb297c493ec9
Napoleon <Napoleon@DESKTOP-2A6FCH8.mshome.net>
2018-06-08 11:28:39 +0200

    Merge origin/master into vladimir_formulaires
    Conflicts:
    	Functions/Functions_Formulaires.php
    	index.php

--- index.php
+++ index.php
@@ -11,11 +11,16 @@ and open the template in the editor.
         <link rel="stylesheet" href="../include/css/bootstrap.css">
     </head>
     <body>
-        <h1>Bienvenue sur Nounou</h1>
-        <a href='view/connexion_form.php'>Se connecter</a> <br>
-        <a href='view/inscription_form.php'>S'inscrire</a>
-        <?php
-        // put your code here
-        ?>
+        <div class="row">
+            <div class="col-sm-8">
+                <h1>Bienvenue sur Nounou</h1>
+                <a href='view/connexion_form.php'>Se connecter</a> <br>
+                <a href='view/inscription_form.php'>S'inscrire</a>
+            </div>
+            <div class="col-sm-4">
+                <a href='view/connexion_form.php'>Se connecter 2</a> <br>
+                <a href='view/inscription_form.php'>S'inscrire 2</a>
+            </div>
+        </div>   
     </body>
 </html>
