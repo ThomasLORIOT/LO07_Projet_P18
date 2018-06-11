dd0613f73d3cb9868c767855e84c6ecdefe92c26
Thomas LORIOT <thomas.loriot@utt.fr>
2018-06-11 15:39:28 +0200

    update du fichier Objets

--- Objets/Garde.php
+++ Objets/Garde.php
@@ -11,6 +11,9 @@
  *
  * @author Thomas
  */
+
+require_once '../Functions/Functions_SQL.php';
+
 class Garde {
 
     private $Nounous_idNounous;
@@ -98,7 +101,7 @@ class Garde {
     }
 
     function setNbr_enfants($nbr_enfants) {
-        if ($nbr_enfants > 0 && $nbr_enfants < 10) {
+        if ($nbr_enfants > 0 && $nbr_enfants < 20) {
             $this->nbr_enfants = $nbr_enfants;
         }
     }
@@ -116,14 +119,53 @@ class Garde {
     function setAppréciation($Appréciation) {
         $this->Appréciation = $Appréciation;
     }
-
-    function __construct($Régulier, $DateD, $DateF, $Lanque, $nbr_Enfants, $Appréciation) {
+    
+    function __construct() {
+        $argv = func_get_args();
+        switch (func_num_args()) {
+            case 7:
+                self::__construct1($argv[0], $argv[1], $argv[2], $argv[3], $argv[4], $argv[5], $argv[6]);
+                break;
+            case 2:
+                self::__construct2($argv[0], $argv[1]);
+                break;
+        }
+    }
+    
+    //pour pouvoir bien insérer la date dans DB, il faut ecrire comme un INT Année/Mois/jour tout attaché pas de slash : 19970324 (24 mars 1997)
+    function __construct1($idNounous, $idHoraires, $Régulier, $DateD, $DateF, $Lanque, $Appréciation) {
+        $this->Nounous_idNounous = $idNounous;
+        $this->Horaires_idHoraires = $idHoraires;
         $this->Régulier = $Régulier;
         $this->DateDébut = $DateD;
         $this->DateFin = $DateF;
         $this->Langue = $Lanque;
-        $this->nbr_enfants = $nbr_Enfants;
         $this->Appréciation = $Appréciation;
+    }
+    
+    function __construct2($idHoraire, $idNounous){
+        $myDB = connectDB();
+        $result = $myDB->query("SELECT * FROM garde WHERE Horaires_idHoraires = '$idHoraire' AND Nounous_idNounous = '$idNounous'");
+        if ($result->num_rows == 0) {
+            echo "<script>console.log('connait pas cette garde');</script>";
+        } else {
+            $row = mysqli_fetch_row($result);
+            $this->setConstruct($row); //magouille pour pouvoir dépasser les 20 lignes autorisées pour un constructeur
+        }
+    }    
+    
+    //magouille pour pouvoir dépasser les 20 lignes autorisées pour un constructeur
+    private function setConstruct($row){
+        $this->Nounous_idNounous = $row[0];
+        $this->Horaires_idHoraires = $row[1];
+        $this->Régulier = $row[2];
+        $this->DateDébut = $row[3];
+        $this->DateFin = $row[4];
+        $this->Langue = $row[5];
+        $this->nbr_enfants = $row[6];
+        $this->Prix = $row[7];
+        $this->Note = $row[8];
+        $this->Appréciation = $row[9];
     }
 
     private function calculPrix() {
--- Objets/Horaires.php
+++ Objets/Horaires.php
@@ -11,6 +11,9 @@
  *
  * @author Thomas
  */
+
+require_once '../Functions/Functions_SQL.php';
+
 class Horaires {
     private $idHoraires;
     private $Date;
@@ -49,13 +52,52 @@ class Horaires {
         $this->HeureFin = $HeureFin;
     }
 
-    function __construct($Date, $HeureD, $HeureF) {
+    function __construct() {
+        $argv = func_get_args();
+        switch (func_num_args()) {
+            case 3:
+                self::__construct1($argv[0], $argv[1], $argv[2]);
+                break;
+            case 1:
+                self::__construct2($argv[0]);
+                break;
+        }
+    }
+    
+    //pour pouvoir bien insérer la date dans DB, il faut ecrire comme un INT Année/Mois/jour tout attaché pas de slash : 19970324 (24 mars 1997)
+    //                          une heure                                    Heure/min/sec                             : 163000 (16h30min0sec)                      
+    function __construct1($Date, $HeureD, $HeureF) {
         $this->Date = $Date;
         $this->HeureDébut = $HeureD;
         $this->HeureFin = $HeureF;
     }
     
+    function __construct2($idHoraire){
+        $myDB = connectDB();
+        $result = $myDB->query("SELECT * FROM horaires WHERE idHoraires = '$idHoraire'");
+        if ($result->num_rows == 0) {
+            echo "<script>console.log('connait pas cet horaire');</script>";
+        } else {
+            $row = mysqli_fetch_row($result);
+            $this->idHoraires = $idHoraire;
+            $this->Date = $row[1];
+            $this->HeureDébut = $row[2];
+            $this->HeureFin = $row[3];
+        }
+    }
+        
     function __toString() {
-        return "Horaire($this->Date;$this->HeureDébut;$this->HeureFin)";
+        return "Horaire($this->idHoraires;$this->Date;$this->HeureDébut;$this->HeureFin)";
+    }
+    
+    function addDB(){
+        $requete = "INSERT INTO horaires(Date, `Heure Début`, `Heure Fin`) VALUES ($this->Date, '$this->HeureDébut', '$this->HeureFin')";
+        requete($requete);
+    }
+    
+    function updateDB(){
+        $requete = "UPDATE horaires SET Date='$this->Date', `Heure Début`='$this->HeureDébut', `Heure Fin`='$this->HeureFin' WHERE idHoraires = $this->idHoraires";
+        requete($requete);
     }
 }
+
